e04acd1e8a6f30e826ab8f34aa181c4aa6ea3a8a
Fredrik Haglund <92490571+fr3h4g@users.noreply.github.com>
2024-06-28 22:37:26 +0000

    update

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 ln -sf "$PWD/files/.config/nixpkgs/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 nix-env -iA nixpkgs.myPackages
-stow --restow --target="$HOME" files
+unlink "$XDG_CONFIG_HOME"/nixpkgs/config.nix
+"$HOME"/.nix-profile/bin/stow --restow --target="$HOME" files
